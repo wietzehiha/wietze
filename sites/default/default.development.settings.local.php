524b40a5fb84bbdc6661280ab4c1eb56d2f65c46
Wietze Eilander <wietze@vdmi.nl>
2016-10-25 16:48:36 +0200

    first commit

---
+++ sites/default/default.development.settings.local.php
@@ -0,0 +1,13 @@
+<?php
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'wietze',
+  'username' => 'root',
+  'password' => 'root',
+  'host' => 'mysql',
+  'prefix' => '',
+);
+
+# Enable on development.
+# $settings['container_yamls'][] = __DIR__ . '/services.development.yml';
---
+++ sites/default/settings.local.php
@@ -0,0 +1,13 @@
+<?php
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'DB_NAME',
+  'username' => 'DB_USER',
+  'password' => 'DB_PASSWORD',
+  'host' => 'localhost',
+  'prefix' => '',
+);
+
+# Enable on development.
+# $settings['container_yamls'][] = __DIR__ . '/services.development.yml';
